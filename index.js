1d972fcfef0952991097a33274464d4c41806031
Jake Streb <jake@getgrist.com>
2017-05-10 15:02:25 -0400

    Added index

---
+++ index.js
@@ -0,0 +1,23 @@
+// Retrieves Grist data for embedding
+function retrieveData(callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://k0yj0ojogc.execute-api.us-east-1.amazonaws.com/test/queryData?embedId=e6I4O2O6X88J4', true);
+  xhr.onload = function (e) {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText);
+        callback(data);
+      } else {
+        console.error(xhr.statusText);
+      }
+    }
+  };
+  xhr.onerror = function (e) {
+    console.error(xhr.statusText);
+  };
+  xhr.send(null);
+}
+
+retrieveData(data => {
+  return data;
+})
