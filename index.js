e599087e11297518bb3cf0d1d6dc5b28bce7b389
Jake Streb <jake@getgrist.com>
2017-05-10 15:13:09 -0400

    fkqwop

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function retrieveData(callback) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://k0yj0ojogc.execute-api.us-east-1.amazonaws.com/test/queryData?embedId=e6I4O2O6X88J4', true);
   xhr.onload = function (e) {
+    console.warn('loaded', e);
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
         let data = JSON.parse(xhr.responseText);
